3e7db534e838069398dc53d56b80344c2a3da085
zadykian <zadykian@outlook.com>
2021-08-05 15:08:27 +0300

    Chapter1

--- src/PlConcepts.Exercises.Chapter1/Types.fs
+++ src/PlConcepts.Exercises.Chapter1/Types.fs
@@ -9,8 +9,14 @@ type binaryOp =
     | Min
     | Equals
 
+/// Variable name.
+type VarName = string
+
 /// Abstract arithmetic expression.
 type expr =
-    | CstI of int
-    | Var  of string
-    | Prim of binaryOp * expr * expr
+    | Constant of int
+    | Var      of VarName
+    | Binary   of binaryOp * expr * expr
+
+/// Evaluation environment which contains named variables.
+type env = (VarName * int) list    
