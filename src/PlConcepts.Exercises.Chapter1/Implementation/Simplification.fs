29678bc3ff9f9f1a3fafa58b0e9f92abbd18cb11
zadykian <zadykian@outlook.com>
2021-08-06 20:13:09 +0300

    simplification

---
+++ src/PlConcepts.Exercises.Chapter1/Implementation/Simplification.fs
@@ -0,0 +1,6 @@
+module PlConcepts.Exercises.Chapter1.Simplification
+
+open PlConcepts.Exercises.Chapter1.Types
+
+/// If possible, simplify expression 'expr'.
+let simplify (expr: Expr): Expr = failwith "not implemented!"
---
+++ src/PlConcepts.Exercises.Chapter1/Tests/SimplificationTests.fs
@@ -0,0 +1,7 @@
+module PlConcepts.Exercises.Chapter1.Tests.SimplificationTests
+
+open FsUnit
+open NUnit.Framework
+
+open PlConcepts.Exercises.Chapter1.Types
+open PlConcepts.Exercises.Chapter1.Simplification
