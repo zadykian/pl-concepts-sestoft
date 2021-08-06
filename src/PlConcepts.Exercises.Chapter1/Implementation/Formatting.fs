ec2980df5c18bdfd1d4e22d207ef644a442b754e
zadykian <zadykian@outlook.com>
2021-08-06 18:55:16 +0300

    expression formatting

--- src/PlConcepts.Exercises.Chapter1/Implementation/Formatting.fs
+++ src/PlConcepts.Exercises.Chapter1/Implementation/Formatting.fs
@@ -2,6 +2,25 @@ module PlConcepts.Exercises.Chapter1.Formatting
 
 open PlConcepts.Exercises.Chapter1.Types
 
+/// Binary operator priority.
+type private OpPriority = byte
+
+/// Binary operator associativity.
+type private OpAssociativity =
+    | Left
+    | Right
+    | Invariant
+
+/// Definitions of operators' behaviour:
+/// relative priority and associativity.
+let private opDefinitions: Map<BinaryOp, OpPriority * OpAssociativity> =
+    [
+        Plus,     (5uy, Invariant)
+        Minus,    (5uy, Left)
+        Multiply, (6uy, Invariant)
+    ]
+    |> Map.ofList
+
 /// Get string representation of 'expr' expression.
 let rec format (expr: Expr): string =
     match expr with
