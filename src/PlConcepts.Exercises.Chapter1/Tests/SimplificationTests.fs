e5f8901c623ff6559ecdc4f8b53cf4a21dd47421
zadykian <zadykian@outlook.com>
2021-08-06 20:54:13 +0300

    SimplificationTests

--- src/PlConcepts.Exercises.Chapter1/Tests/SimplificationTests.fs
+++ src/PlConcepts.Exercises.Chapter1/Tests/SimplificationTests.fs
@@ -6,12 +6,25 @@ open NUnit.Framework
 open PlConcepts.Exercises.Chapter1.Types
 open PlConcepts.Exercises.Chapter1.Simplification
 
+type TestCase = Expr * Expr
+
 /// Simplify expression 'expr' and compare result with 'expected'.
 let private assertSimplification (input: Expr) (expected: Expr) =
     simplify input |> should equal expected
 
-[<Test>]
-let ``constant can't be more simple`` () =
-    let expression = Constant 32
-    assertSimplification expression (Constant 32)
+let private zero = Constant 0
+let private one  = Constant 1
 
+let private testCases =
+    seq {
+        Constant 32, Constant 32
+        Binary (Plus, zero, Var "x"), Var "x"
+    }
+    |> Seq.map (fun (i, e) -> [|i; e|])
+    |> Seq.toArray
+
+[<Test>]
+[<TestCaseSource(nameof testCases)>]
+let ``run all test cases`` (testCase: TestCase) =
+    let input, expected = testCase
+    assertSimplification input expected
