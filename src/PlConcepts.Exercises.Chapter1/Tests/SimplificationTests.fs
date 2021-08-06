448f4459e0c6a7f90fc9cabad2ca65ee544792de
zadykian <zadykian@outlook.com>
2021-08-06 20:18:19 +0300

    *

--- src/PlConcepts.Exercises.Chapter1/Tests/SimplificationTests.fs
+++ src/PlConcepts.Exercises.Chapter1/Tests/SimplificationTests.fs
@@ -7,10 +7,10 @@ open PlConcepts.Exercises.Chapter1.Types
 open PlConcepts.Exercises.Chapter1.Simplification
 
 /// Simplify expression 'expr' and compare result with 'expected'.
-let private assertSimpl (input: Expr) (expected: Expr) =
+let private assertSimplification (input: Expr) (expected: Expr) =
     simplify input |> should equal expected
 
 [<Test>]
 let ``constant can't be more simple`` () =
     let expression = Constant 32
-    assertSimpl expression (Constant 32)
+    assertSimplification expression (Constant 32)
